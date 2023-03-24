ffd0ee76a295496f097498ef652d2d3350068a7c
unknown <josepc100@gmail.com>
2023-03-23 21:11:55 -0300

    inicializo proyecto

---
+++ CursoJavaNivelBasico/src/Aprendizaje/Practica_1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aprendizaje;
+
+/**
+ *
+ * @author JosePc
+ */
+public class Practica_1 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ CursoJavaNivelBasico/src/Aprendizaje/Variables.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Aprendizaje;
+
+/**
+ *
+ * @author JosePc
+ */
+public class Variables {
+    public static void main(String[] args) {
+        int Edad;
+        String Nombre;
+        
+    }
+}
